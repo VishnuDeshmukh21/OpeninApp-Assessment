40677bbe82789a093ae66987a7d263e6d639fc55
vishnu <vishnudeshmukh1222@gmail.com>
2023-12-28 03:29:44 +0530

    Implemented automatic reply with Gmail API

---
+++ credentials.js
@@ -0,0 +1,7 @@
+PORT=8000
+CLIENT_ID="528283222028-1tniusal4brf60g2kcjl746g2aearpgj.apps.googleusercontent.com"
+CLIENT_SECRET="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"
+REDIRECT_URI="https://developers.google.com/oauthplayground"
+REFRESH_TOKEN="1//04v5oldBuIZlpCgYIARAAGAQSNwF-L9IrM6JpXh24hE-1O8oN3GdGfjwXgzSBL2bE0MIV4P2m8DYv9woKYuXY17oxtavfZ6E_x2o"
+
+module.exports = { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
